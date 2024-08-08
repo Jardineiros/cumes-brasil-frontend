e5d9664619c164a643ad0fe214c264162498f9c2
Rafael Ventura <raf4ael@gmail.com>
2024-08-08 03:10:35 -0300

    add front

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+html, body, #q-app {
+  height: 100%;
+}
+</style>
---
+++ src/models/Escalada.ts
@@ -0,0 +1,12 @@
+// import { Via } from "src/models/Via";
+// import { Usuario } from "src/models/Usuario";
+import { Participante } from './Participante';
+
+export interface Escalada {
+  // usuario: Usuario;
+  viaId: number;
+  data: string;
+  id?: number;
+  observacao?: string;
+  participantes: Participante[];
+}
